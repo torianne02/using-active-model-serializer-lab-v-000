50495640f4d424ce11c7324e2de968c196c7c765
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-10-17 22:37:00 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -26,7 +26,7 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     respond_to do |format|
       format.html { render :show }
-      format.json { render json: @product, status: 200 }
+      format.json { render json: @product }
     end
   end
 
